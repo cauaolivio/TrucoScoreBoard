0d6fe03d0c5218164fbbabe4e6e767c35523d0e5
Cauã Olivio <caua.olivio@gmail.com>
2026-08-16 14:42:33 -0300

    feat: funcionamento dos botãos de +1, +3 e reiniciar.

--- app/src/main/java/br/edu/ifsp/scl/sc3039587/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3039587/trucoscoreboard/MainActivity.kt
@@ -1,20 +1,49 @@
 package br.edu.ifsp.scl.sc3039587.trucoscoreboard
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.sc3039587.trucoscoreboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private var nosScore: Int = 0
+    private var elesScore: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(activityMainBinding.root)
+
+        with(activityMainBinding) {
+            btNosIncrementar1.setOnClickListener {
+                nosScore++
+                tvNosCounter.text = nosScore.toString()
+            }
+
+            btNosIncrementar3.setOnClickListener {
+                nosScore += 3
+                tvNosCounter.text = nosScore.toString()
+            }
+
+            btElesIncrementar1.setOnClickListener {
+                elesScore++
+                tvElesCounter.text = elesScore.toString()
+            }
+
+            btElesIncrementar3.setOnClickListener {
+                elesScore += 3
+                tvElesCounter.text = elesScore.toString()
+            }
+
+            btReiniciar.setOnClickListener {
+                nosScore = 0
+                elesScore = 0
+                tvNosCounter.text = "0"
+                tvElesCounter.text = "0"
+            }
         }
     }
 }
